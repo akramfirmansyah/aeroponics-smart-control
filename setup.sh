717a77d80a97fe3951199bc80ba3f3b6c981b7be
akramfirmansyah <akram.firman@gmail.com>
2025-10-22 21:03:00 +0800

    fix example env

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ mkdir -p mosquitto/log
 mkdir -p influxdb/data
 mkdir -p influxdb/config
 
+sudo apt update && sudo apt install npm -y
+
 cd node-red && npm install && cd ..
 
 docker compose --project-name sgh up -d
